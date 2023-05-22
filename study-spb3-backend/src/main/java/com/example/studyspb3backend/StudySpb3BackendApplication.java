5eb1acf0725cb66cc17c36e41962c6c68ca99996
linxin4cs <linxin4cs@gmail.com>
2023-05-22 09:35:52 +0800

    Init: 创建项目

---
+++ study-spb3-backend/src/main/java/com/example/studyspb3backend/StudySpb3BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyspb3backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpb3BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpb3BackendApplication.class, args);
+    }
+
+}
---
+++ study-spb3-backend/src/test/java/com/example/studyspb3backend/StudySpb3BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyspb3backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpb3BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
